953e33f47d8518c9c13e4a775a0de25905be214e
jhyeong <hyeongcode@gmail.com>
2025-03-12 23:52:35 +0900

    feat: CORS config add, edit

--- src/main/java/org/hyeong/gooinprochatapi/config/CorsConfig.java
+++ src/main/java/org/hyeong/gooinprochatapi/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns("*") // 모든 출처 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "FETCH", "HEAD", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 인증 정보 포함 (쿠키, Authorization 헤더 등)
+                .allowCredentials(false); // 인증 정보 포함 (쿠키, Authorization 헤더 등) || .allowedOriginPatterns("*") 할 때 true 쓰면 설정 꼬임(주의)
     }
 }
 
