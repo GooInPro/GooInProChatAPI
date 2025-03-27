d54a50215e6bb320f0c81344b7ea7488a997044f
jhyeong <hyeongcode@gmail.com>
2025-03-27 12:38:27 +0900

    WebSocketConfig add withSocketJS

--- src/main/java/org/hyeong/gooinprochatapi/config/WebSocketConfig.java
+++ src/main/java/org/hyeong/gooinprochatapi/config/WebSocketConfig.java
@@ -20,6 +20,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/ws").setAllowedOrigins("*"); //CORS 문제 해결 위해 *
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .withSockJS(); //CORS 문제 해결 위해 *
     }
 }
